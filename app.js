38ce58682bf4718c2a36dfd091efc61602f06ff5
Phitron Batch 3 <rahatkhanpathan99@gmail.com>
2023-03-23 02:24:41 +0600

    app done

---
+++ app.js
@@ -0,0 +1,45 @@
+const yargs = require("yargs");
+const notes = require("./note.js");
+yargs.command({
+    command: "add",
+    description: "Add a note",
+    builder: {
+        title: {
+            description: "Give title",
+            demandOption: true,
+            type: "string",
+        },
+        body: {
+            description: "Give body",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body);
+    },
+});
+yargs.command({
+    command: "remove",
+    description: "Remove a note",
+    builder: {
+        title: {
+            description: "Give title",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title);
+    },
+});
+
+yargs.command({
+    command: "view",
+    description: "View all notes",
+    handler: function () {
+        notes.viewNotes();
+    },
+});
+
+yargs.parse();
---
+++ note.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const getNotes = function () {
+    try {
+        dataBuffer = fs.readFileSync("./data.json");
+        jsonData = dataBuffer.toString();
+        data = JSON.parse(jsonData);
+        return data;
+    } catch (e) {
+        return [];
+    }
+};
+const addNote = function (title, body) {
+    data = getNotes();
+    const duplicate = data.filter(function (note) {
+        return note.title === title;
+    });
+    if (duplicate.length > 0) {
+        console.log("Duplicate title");
+        return;
+    }
+    data.push({ title: title, body: body });
+    updateData(data);
+    console.log("Note added");
+};
+const removeNote = function (title) {
+    data = getNotes();
+    const found = data.filter(function (note) {
+        return note.title === title;
+    });
+    if (found.length === 0) {
+        console.log("Note not found");
+    } else {
+        data.splice(data.indexOf(found), 1);
+        updateData(data);
+        console.log("Note removed");
+    }
+};
+const viewNotes = function () {
+    data = getNotes();
+    let i = 1;
+    data.forEach((e) => {
+        console.log(`Note - ${i++}\nTitle-${e.title}\nBody-${e.body}\n`);
+    });
+};
+const updateData = function (data) {
+    fs.writeFileSync("./data.json", JSON.stringify(data));
+};
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    viewNotes: viewNotes,
+};
